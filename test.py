1ec899b6969ed11211d3ff4721b7631bd9ca538d
Gabriel <beauchampgabriel@gmail.com>
2014-09-24 20:26:36 -0400

    Fuck le setup

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ class Vue:
         self.panmenu.add(Button(self.root, text='Quitter',command=self.quitter))
         self.panmenu.add(Button(self.root, text='Deplace', command=self.deplacement))
         self.panmenu.pack()
-
+        
     def quitter(self):
         self.root.destroy()
         exit(0)
@@ -24,19 +24,16 @@ class Vue:
     def toggleMur(self, event):
         self.p.m.map[int(event.x/20)][int(event.y/20)].togglepassable()
         
-    def addWayPoint(self, event):
-        print('Ok: ', event.x, event.y)
-        self.p.m.joueur.chemin.append(Case(event.x, event.y))
-
     def deplacement(self):
-        self.p.m.joueur.deplacement()
+        self.p.m.joueur.deplace(self.p.m.joueur.depart, self.p.m.joueur.arrivee)
         self.root.after(50, self.deplacement)
 
     def setDepart(self, event):
-        self.p.m.joueur.depart = Case(event.x, event.y)
+        self.p.m.joueur.depart.x = event.x
+        self.p.m.joueur.depart.y = event.y
 
     def setArrivee(self, event):
-        self.p.m.joueur.arrivee = Case(event.x, event.y)
+        self.p.m.joueur.arrivee = Noeud(event.x, event.y, True, 0, None)
 
     def addMur(self,event):
         print(event.x)
@@ -51,11 +48,11 @@ class Vue:
         self.can.delete("piece")
         self.can.delete("but")
         self.can.delete("mur")
-        self.can.create_text(self.p.m.joueur.depart.x, self.p.m.joueur.depart.y, text= "@", tags="piece")
-        self.can.create_text(self.p.m.joueur.arrivee.x, self.p.m.joueur.arrivee.y, text= "X", tags="but")
+        self.can.create_text(self.p.getjx(), self.p.getjy(), text= "@", tags="piece")
+        self.can.create_text(self.p.getjarrx(), self.p.getjarry(), text= "X", tags="but")
         for i in range(25):
             for j in range(25):
-                if self.p.m.map[i][j].passable == False:
+                if self.p.m.map[i][j].passablep() == False:
                     self.can.create_text(i*20 + 10, j*20 + 10, text = "=", tags = "mur")
                     
     def jouer(self):
@@ -64,32 +61,60 @@ class Vue:
         self.can.pack()
         self.root.after(200,self.jouer)
 
-class Case:
-    def __init__(self, x, y):
+class Noeud:
+    def __init__(self, x, y, passable, poid, parent):
         self.x = x
         self.y = y
-
-class Noeud:
-    def __init__(self, pt, passable, poid, parent):
-        self.pt = pt
         self.passable = passable
         self.poid = poid        # Ou on le calcule a partir du parent?
         self.parent = parent    # Noeud precedent. (D'ou on vient)
 
-    def x(self):
-        return self.pt.x
-    def y(self):
-        return self.pt.y
+    # def x (self):
+    #     return self.pt.x
+    # def y (self):
+    #     return self.pt.y
 
     def passablep(self):
         return self.passable
-    
+
+    def togglepassable(self):
+        self.passable = not self.passable
+        
 class Joueur:
     def __init__(self, p):
         self.p = p              # Modele
+        self.depart = Noeud(0, 0, True, 0, None)
+        self.arrivee = Noeud(0, 0, True, 0, None)
+
+    def aetoile(self, dep, arr):
+        """ dep c'est un noeud de depart, et arr c'est un point d'arrivee (un noeud au pire) """
+        open = [dep]
+        close = []
+        current = None
+        g = {}                  # Cost_so_far
+        g[dep] = 0
+        came_from = {}
+        
+        while open:
+            current = open[0]   # .pop()?
+            
+            # early exit
+            if len(open) >= 10:
+                return came_from
+
+            for next in self.voisins(current):
+                print("Dans next", next)
+                f = g[nex] + h(current, next)
+                if next not in g or f < g[next]:
+                    g[next] = f
+                    # priority = f + g(arr, next)
+                    open.append(next)
+                    came_from[next] = current
+        return came_from
 
     def deplace(self, dep, arr):
-        path = aetoile(dep,arr)
+        path = self.aetoile(dep,arr)
+        print("After a*")
         if self.h(dep, arr) == 0:
             if path:
                 del path[0]
@@ -108,30 +133,7 @@ class Joueur:
             if dep.x > arr.x:
                 dep.x -= 1
             if dep.y > arr.y:
-                dep.y -= 1
-        
-    def aetoile(self, dep, arr):
-        """ dep c'est un noeud de depart, et arr c'est un point d'arrivee (un noeud au pire) """
-        open = [dep]
-        close = []
-        current = None
-        g = {}                  # Cost_so_far
-        g[dep] = 0
-        came_from = {}
-        
-        while open:
-            current = open[0]   # .pop()?
-
-            # early exit
-
-            for next in self.voisins(current):
-                f = g[nex] + h(current, next)
-                if next not in g or f < g[next]:
-                    g[next] = f
-                    # priority = f + g(arr, next)
-                    open.append(next)
-                    came_from[next] = current
-        return came_from
+                dep.y -= 1        
     
     def h(a, b):
         x1 = a.x()
@@ -140,38 +142,31 @@ class Joueur:
         y2 = b.y()
         return abs(x1 - x2) + abs(y1 - y2)
 
-    def voisins(n):             # Pardon de la mochete.
+    def voisins(self,n):
         """Retourne les voisins d'un point, ignorant tout ce qui est pas `passable'""" 
         voisin = []
-        try:
-            voisin.append(self.p.map[n.x()][n.y()-1])
-        except IndexError:
-            pass
-        try:
-            voisin.append(self.p.map[n.x()][n.y()+1])
-        except IndexError:
-            pass
-        try:
-            voisin.append(self.p.map[n.x()+1][n.y()])
-        except IndexError:
-            pass
-        try:
-            voisin.append(self.p.map[n.x()-1][n.y()])
-        except IndexError:
-            pass
+        for i in [0,1,-1]:
+            for j in [-1,1,0]:
+                try:
+                    voisin.append(self.p.map[n.x+i][n.y+j])
+                    print("No fuck")
+                except IndexError:
+                    print("Fuck")
+        print(list(voisin))
+        voisin = filter(self.p.n.passablep, voisin)
 
-        voisin = filter(self.p.n.passablep, voisin) # Donner un qui est dans le voisin? ._.
+        return list(voisin)
         
 class Modele:
     def __init__(self, parent):
         self.parent = parent
-        self.n = Noeud(None, True, 0, None)
-        self.joueur = Pion(self, Case(0,0))
+        self.n = Noeud(0, 0, True, 0, None)
+        self.joueur = Joueur(self)
         self.map = []
         for i in range(25):
             self.map.append([])
             for j in range(25):
-                self.map[i].append(Case(i,j))
+                self.map[i].append(Noeud(i, j, True, 0, None))
                 
         
 class Controleur:
@@ -181,4 +176,17 @@ class Controleur:
         self.vue.jouer()
         self.vue.root.mainloop()
 
+    def getjx(self):
+        return self.m.joueur.depart.x
+
+    def getjy(self):
+        return self.m.joueur.depart.y
+
+    def getjarrx(self):
+        return self.m.joueur.arrivee.x
+
+    def getjarry(self):
+        return self.m.joueur.arrivee.y
+
+    
 jeu = Controleur()
