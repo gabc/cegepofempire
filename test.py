ba8c6bd509269b35618d7c475a0b03023fd54766
Gabriel Beauchamp <beauchampgabriel@gmail.com>
2014-09-27 18:44:58 -0400

    Clunky, mais fonctionne \o/

--- test.py
+++ test.py
@@ -1,73 +1,121 @@
 #!/usr/bin/env python3
-# class Point:
-#     def __init__(self,x,y,poid,passable, parent):
-#         self.x = x
-#         self.y = y
-#         self.poid = poid
-#         self.passable = passable
-#         self.parent = parent
+class Noeud:
+    def __init__(self, x, y, f, gc, parent):
+        self.x = x
+        self.y = y
+        self.f = f
+        self.gc = gc
+        self.parent = parent
         
-#     def togglepassable(self):
-#         self.passable = not self.passable
-
-#     def passablep(self):
-#         return self.passable
-
+class Point:
+    def __init__(self, x, y, flag):
+        self.x = x
+        self.y = y
+        self.flag = flag
 
 # Fuck that
 # 0 = passable
 # 1 = depart
 # 2 = arrive
 # 3 = mur
+# 4 = chemin
 
 map = []
 path = []
-depart = (2,2)
-arrivee = (5,2)
+depart = Noeud(1, 1, 0, 0, None)
+arrivee = Noeud(5, 6, 0, 0, None)
+trouble = [(3,2),(3,3),(3,4),(3,5), (4,6)]
+
 PASSABLE = 3
+
 def initmap():
     for i in range(25):
         map.append([])
         for j in range(25):
-            map[i].append(0)
-
+            map[i].append(Point(i,j,0))
+    map[depart.x][depart.y].flag = 1
+    map[arrivee.x][arrivee.y].flag = 2
+    for i in trouble:
+        map[i[0]][i[1]].flag = 3
+    
 def printmap():
     for i in range(25):
         t = ""
         for j in range(25):
-            if map[i][j] == 0:
+            if map[i][j].flag == 0:
                 t+="."
-            elif map[i][j] == 1:
+            elif map[i][j].flag == 1:
                 t+="D"
-            elif map[i][j] == 2:
+            elif map[i][j].flag == 2:
                 t+="A"
-            elif map[i][j] == 3:
+            elif map[i][j].flag == 3:
                 t+="-"
+            elif map[i][j].flag == 4:
+                t+="+"
         print(t)
 
-def voisin(xy):
-    x = xy[0][0]
-    y = xy[0][1]
-    rep = [(0,0)]
+def voisin(n):
+    x = n.x
+    y = n.y
+    rep = []
     for i in (-1,1,0):
         for j in (0,1,-1):
             try:
-                if map[x+i][y+j] != 3 and (i != 0 or j != 0) :  # Si c'est passable et que les deux i,j sont pas 0.
-                    rep.append((x+i,y+j))
+                if map[x+i][y+j].flag != 3 and (i != 0 or j != 0) :  # Si c'est passable et que les deux i,j sont pas 0.
+                    rep.append(Noeud(x+i, y+j, 0, 0, n))
             except IndexError:
                 pass
     return rep
 
-def distance(a, b):
-    (x1, y1) = a[0]
-    (x2, y2) = b
+def h(a, b):
+    x1, y1 = a.x, a.y
+    x2, y2 = b.x, b.y
     return abs(x1 - x2) + abs(y1 - y2)
 
-def chemin():
-
-
-# Theoriquement, le `main'
-initmap()
-foo, cost = chemin()
-print(foo)                     
+def g(n):
+    acc = 0
+    while n.parent is not None:
+        acc += n.gc
+        n = n.parent
+    return acc
+        
+def path(n):
+    path = []
+    while n.parent is not None:
+        path = [n.parent] + path
+        n = n.parent
+    return path
     
+def chemin():
+    open = [depart]
+    closed = []
+
+    while open:
+        current = open[0]
+        del open[0]
+        closed.append(current)
+
+        if h(current, arrivee) == 0:
+            return path(current)
+
+        for v in voisin(current):
+            f = g(v) + h(v,arrivee)
+            v.parent = current  # Je pense que je le fais déjà dans voisin(), mais dans le doute
+            v.f = f             # Meeeh
+            v.gc = g(v)             # MEEEEH
+
+            # if v in open or v in closed and v.f Si il est dans open, ou closed. Et que f est plus petit que l'autre.
+            #  open.append(v) Le remetre dans open.. basically.
+
+            if v not in open and v not in closed:
+                open.append(v)
+            open.sort(key = lambda x: x.f)
+                
+## Theoriquement, le `main'
+initmap()
+printmap()
+foo = chemin()
+for i in foo:
+    print(i.x, i.y)
+    map[i.x][i.y].flag = 4
+printmap()
